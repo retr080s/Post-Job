f8a2af7eec9d2d144d6d6fcb01d06ad3c7e202d8
retr080s <retr080s@protonmail.com>
2023-10-10 17:13:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Job post</title>
+</head>
+<body>
+     <div class="header">
+  <a href="#default" class="logo">PostJob</a>
+  <div class="header-right">
+    <a class="active" href="#home">Post a Job</a>
+    <a href="#contact">All Jobs</a>
+    <a href="#about">About</a>
+  </div>
+</div> 
+</body>
+</html>
