45fc5cca12574d4b4548c014ddeb73b9d9363a0a
retr080s <retr080s@protonmail.com>
2023-10-11 01:04:13 +0200

    favicon made and added

--- includes/links.php
+++ includes/links.php
@@ -1,4 +1,5 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<link rel=" icon" type="image/x-icon" href="PostJob/favicon.ico">
     <link rel="stylesheet" href="style.css">
